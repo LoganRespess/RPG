d49b65f80644f3c14ed8dcc82891db086a6ac242
Logan <Logan.respess@gmail.com>
2020-06-24 16:58:44 -0500

    finsihed up to episode 10, attack animation, breakable grass and a grass effect added

--- Godot/Player/Player.gd
+++ Godot/Player/Player.gd
@@ -4,6 +4,15 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+
+#state machine
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+var state = MOVE
+
 var velocity = Vector2.ZERO
 
 #Animation
@@ -15,7 +24,19 @@ func _ready():
 	animationTree.active = true
 
 #Movement function
-func _physics_process(delta):
+func _process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+			
+		ROLL:
+			pass
+			
+		ATTACK:
+			attack_state(delta)
+	
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -24,6 +45,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -31,3 +53,12 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("Attack"):
+		state = ATTACK
+	
+func attack_state(delta):
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
